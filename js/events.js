751bcae53377a0e8460fa33ec1699cf31f502fd1
Pitta <mailez4all@gmail.com>
2018-02-28 09:09:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,18 @@
 //define functions here
 
+function getIt() {
+
+  alert("Hey!");
+  return;
+  
+}
+
 $(document).ready(function(){
 
+$('p'.on("click", function() {
+
+  
+}))
 // call functions here
 
 });
