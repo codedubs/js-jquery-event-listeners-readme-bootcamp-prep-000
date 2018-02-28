8bfd82a4ff608185ea684947fee3b3c8d66231c5
Pitta <mailez4all@gmail.com>
2018-02-28 10:15:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -26,7 +26,16 @@ function pressIt() {
     }
   });
 
+}
 
+function submitIt() {
+
+  $('form').on('submit', function() {
+
+    alert("Your form is going to be submitted now.")
+    return;
+  })
+  
 }
 
 
@@ -34,7 +43,7 @@ $(document).ready(function() {
 
 getIt();
 
-
+pressIt();
 // call functions here
 
 });
